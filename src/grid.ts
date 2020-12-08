5fcf93321c5a3fc49f2f12bc6a7d15169edb400e
Nick Matantsev <nick.matantsev@gmail.com>
2020-12-07 20:44:44 -0500

    Move out tile mesh definition

---
+++ src/grid.ts
@@ -0,0 +1,8 @@
+export const GRID_CELL_SIZE = 2;
+
+export const CARDINAL_DIR_LIST = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1]
+];
--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -5,11 +5,9 @@ import { Canvas } from 'react-three-fiber';
 import { MapControls } from '@react-three/drei/MapControls';
 import { Line } from '@react-three/drei/Line';
 import * as THREE from 'three';
-import { CSG } from '@jscad/csg';
 
-const {
-  Geometry: CSGGeometry
-} = require('../sk-csg-modeler/packages/modeler-csg/src/index');
+import { GRID_CELL_SIZE, CARDINAL_DIR_LIST } from './grid';
+import { TileMesh } from './TileMesh';
 
 export default {
   title: 'Basic scene',
@@ -20,14 +18,6 @@ export default {
 
 const GRID_WIDTH = 4;
 const GRID_HEIGHT = 4;
-const GRID_CELL_SIZE = 2;
-
-const CARDINAL_DIR_LIST = [
-  [1, 0],
-  [0, 1],
-  [-1, 0],
-  [0, -1]
-];
 
 const TileMeshPreview: React.FC<{
   x: number;
@@ -53,57 +43,6 @@ const TileMeshPreview: React.FC<{
           lineWidth={2}
         />
       </mesh>
-    </>
-  );
-};
-
-const TileMesh: React.FC<{
-  x: number;
-  y: number;
-  entry: number;
-  exit: number;
-}> = ({ entry, exit, x, y }) => {
-  const [px, py] = CARDINAL_DIR_LIST[entry];
-  const [nx, ny] = CARDINAL_DIR_LIST[exit];
-
-  return (
-    <>
-      <group position={[x * GRID_CELL_SIZE, y * GRID_CELL_SIZE, 0]}>
-        <mesh castShadow receiveShadow>
-          <CSGGeometry>
-            <cube
-              color={[1, 0, 0]}
-              center={[0, 0, 0.25]}
-              radius={[GRID_CELL_SIZE * 0.25, GRID_CELL_SIZE * 0.25, 0.25]}
-            />
-            <cube
-              center={[
-                nx * 0.375 * GRID_CELL_SIZE,
-                ny * 0.375 * GRID_CELL_SIZE,
-                0.25
-              ]}
-              radius={[
-                GRID_CELL_SIZE * (0.25 - Math.abs(nx) * 0.125),
-                GRID_CELL_SIZE * (0.25 - Math.abs(ny) * 0.125),
-                0.25
-              ]}
-            />
-            <cube
-              center={[
-                px * 0.375 * GRID_CELL_SIZE,
-                py * 0.375 * GRID_CELL_SIZE,
-                0.25
-              ]}
-              radius={[
-                GRID_CELL_SIZE * (0.25 - Math.abs(px) * 0.125),
-                GRID_CELL_SIZE * (0.25 - Math.abs(py) * 0.125),
-                0.25
-              ]}
-            />
-          </CSGGeometry>
-          <meshLambertMaterial vertexColors />
-        </mesh>
-      </group>
     </>
   );
 };
